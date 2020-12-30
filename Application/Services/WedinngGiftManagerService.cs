8fd4f19d359df9cd13236a2dbcd9abe7a2c65185
Lukáš Drábek <lukas.drabek@vsb.cz>
2020-12-30 14:29:56 +0100

    WedinngGiftManagerService impl

--- Application/Services/WedinngGiftManagerService.cs
+++ Application/Services/WedinngGiftManagerService.cs
@@ -14,24 +14,40 @@ namespace Application.Services
 
         }
 
-        public void AddGiftToList(WeddingGift gift, int wedinngID)
+        public void AddGiftToList(WeddingGift gift, int wedinngID) //FIX proc id v parametrtu ?
         {
-            throw new NotImplementedException();
+            UnitOfWork.WedinngGifts.Add(gift);
+            UnitOfWork.Save();
         }
 
         public void CreateGiftList(IEnumerable<(string Name, string Link)> gifts, int wedinngID)
         {
-            throw new NotImplementedException();
+            foreach (var gift in gifts)
+            {
+                UnitOfWork.WedinngGifts.Add(new WeddingGift()
+                {
+                    WeddingId = wedinngID,
+                    Name = gift.Name,
+                    Link = gift.Link,
+                    IsRegistred = false,
+
+                });
+            }
+            UnitOfWork.Save();
         }
 
-        public bool RegisterGift(string giftId, string userId)
+        public void RegisterGift(int giftId, int userId)
         {
-            throw new NotImplementedException();
+            var gift = UnitOfWork.WedinngGifts.Get(giftId);
+            gift.UserId = userId;
+            UnitOfWork.WedinngGifts.Update(gift);
+            UnitOfWork.Save();
         }
 
         public void RemoveGiftFromList(WeddingGift gift, int wedinngID)
         {
-            throw new NotImplementedException();
+            UnitOfWork.WedinngGifts.Remove(gift);
+            UnitOfWork.Save();
         }
     }
 }
